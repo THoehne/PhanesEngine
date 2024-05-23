3ea1a9b9883c135037816323c75484ff0bc58fba
scorpioblood <77296181+scorpioblood@users.noreply.github.com>
2024-05-23 21:36:24 +0200

    Update includes.

--- Engine/src/Runtime/PhanesEnginePCH.h
+++ Engine/src/Runtime/PhanesEnginePCH.h
@@ -10,34 +10,35 @@
 
     // STL
 
-    #include <cmath>
-    #include <stdint.h>
-    #include <vector>	
-    #include <concepts>
-    #include <type_traits>
-    #include <string>
-
-    #include <iostream>
-    #include <stdio.h>
-
-
-    #include <chrono>
-    #include <thread>
-
-    #ifdef P_WIN_BUILD 
-
-        #include <windows.h>
-
-    #endif
-
-
-
-    // spdlog
-    #include <spdlog/sinks/stdout_color_sinks.h>
-    #include <spdlog/spdlog.h>
-
-    // Local PCH
-    // #include "Core/public/Math/MathPCH.h"
-
+#   include <cmath>
+#   include <stdint.h>
+#   include <vector>	
+#   include <concepts>
+#   include <type_traits>
+#   include <string>
+#   include <memory>
+#
+#   include <iostream>
+#   include <stdio.h>
+#
+#
+#   include <chrono>
+#   include <thread>
+#
+#   ifdef P_WIN_BUILD 
+#
+#       include <windows.h>
+#
+#   endif
+#
+#
+#
+#   // spdlog
+#   include <spdlog/sinks/stdout_color_sinks.h>
+#   include <spdlog/spdlog.h>
+#
+#   // Local PCH
+#   // #include "Core/public/Math/MathPCH.h"
+#
 #endif // !PHANES_CORE_PCH_H
 
