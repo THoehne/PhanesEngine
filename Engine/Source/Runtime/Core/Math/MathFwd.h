af41821541b30b29955bd2f1521b11d6b1f407e7
Thorben Höhne <thorben.hoehne@proton.me>
2025-05-04 21:26:24 +0200

    chore: Replacing typedef with using.

--- Engine/Source/Runtime/Core/Math/MathFwd.h
+++ Engine/Source/Runtime/Core/Math/MathFwd.h
@@ -1,201 +1,238 @@
 #pragma once
 
 #ifdef P_BUILD_LIB
-    #include "PhanesEnginePCH.h"
-#else 
-    #include <vector>
+#	include "PhanesEnginePCH.h"
+#else
+#	include <vector>
 #endif
 
 #ifndef MATH_FWD_H
-#define MATH_FWD_H
+#	define MATH_FWD_H
 
-#include "Core/Math/Boilerplate.h"
-#include "Core/Math/SIMD/PhanesSIMDTypes.h"
+#	include "Core/Math/Boilerplate.h"
+#	include "Core/Math/SIMD/PhanesSIMDTypes.h"
 
 /**
- * Includes forward declarations, as well as certain useful typedefs.
+ * Includes forward declarations, as well as certain useful usings.
  * 
  * @ref OSAL/PlatformTypes.h
  */
 
+namespace Phanes::Core::Math
+{
 
-namespace Phanes::Core::Math {
-
-    /**
+	/**
      * Template forward declarations.
      */
 
-    template<RealType T>    struct TColor;
-    template<RealType T>    struct TLinearColor;
-    template<RealType T>    struct TLine;
-    template<RealType T>    struct TQuaternion;
-    template<RealType T>    struct TTransform;
-    template<RealType T>    struct TPoint2;
-    template<RealType T>    struct TPoint3;
-    template<RealType T>    struct TPoint4;
-    template<IntType T>		struct TIntPoint2;
-    template<IntType T>		struct TIntPoint3;
-    template<IntType T>		struct TIntPoint4;
-    template<RealType T>    struct TMatrix2;
-    template<RealType T, bool S>    struct TMatrix3;
-    template<RealType T, bool S>    struct TMatrix4; 
-    template<RealType T, bool S>    struct TVector2;
-    template<RealType T, bool S>    struct TVector3;
-    template<RealType T, bool S>	struct TVector4;
-    template<IntType T, bool S>		struct TIntVector2;
-    template<IntType T, bool S>		struct TIntVector3;
-    template<IntType T, bool S>		struct TIntVector4;
-    template<RealType T, bool S>    struct TPlane;
-    template<RealType T, bool S>    struct TRay;
+	template <RealType T>
+	struct TColor;
 
-    /**
+	template <RealType T>
+	struct TLinearColor;
+
+	template <RealType T>
+	struct TLine;
+
+	template <RealType T>
+	struct TTransform;
+
+	template <RealType T>
+	struct TPoint2;
+
+	template <RealType T>
+	struct TPoint3;
+
+	template <RealType T>
+	struct TPoint4;
+
+	template <IntType T>
+	struct TIntPoint2;
+
+	template <IntType T>
+	struct TIntPoint3;
+
+	template <IntType T>
+	struct TIntPoint4;
+
+	template <RealType T>
+	struct TMatrix2;
+
+	template <RealType T, bool S>
+	struct TQuaternion;
+
+	template <RealType T, bool S>
+	struct TMatrix3;
+
+	template <RealType T, bool S>
+	struct TMatrix4;
+
+	template <RealType T, bool S>
+	struct TVector2;
+
+	template <RealType T, bool S>
+	struct TVector3;
+
+	template <RealType T, bool S>
+	struct TVector4;
+
+	template <IntType T, bool S>
+	struct TIntVector2;
+
+	template <IntType T, bool S>
+	struct TIntVector3;
+
+	template <IntType T, bool S>
+	struct TIntVector4;
+
+	template <RealType T, bool S>
+	struct TPlane;
+
+	template <RealType T, bool S>
+	struct TRay;
+
+	/**
      * Specific instantiation of forward declarations.
      */
 
-    // TPoint2
+	// TPoint2
 
-    typedef TPoint2<float>      Point2;
-    typedef TPoint2<float>      Point2f;
-    typedef TPoint2<double>     Point2d;
+	using Point2 = TPoint2<float>;
+	using Point2f = TPoint2<float>;
+	using Point2d = TPoint2<double>;
 
-    // TPoint3
+	// TPoint3
 
-    typedef TPoint3<float>      Point3;
-    typedef TPoint3<float>      Point3f;
-    typedef TPoint3<double>     Point3d;
+	using Point3 = TPoint3<float>;
+	using Point3f = TPoint3<float>;
+	using Point3d = TPoint3<double>;
 
-    // TPoint4
+	// TPoint4
 
-    typedef TPoint4<float>      Point4;
-    typedef TPoint4<float>      Point4f;
-    typedef TPoint4<double>     Point4d;
+	using Point4 = TPoint4<float>;
+	using Point4f = TPoint4<float>;
+	using Point4d = TPoint4<double>;
 
+	// TIntPoint2
 
-    // TIntPoint2
+	using IntPoint2 = TIntPoint2<int>;
+	using IntPoint2i = TIntPoint2<int>;
+	using IntPoint2l = TIntPoint2<long>;
 
-    typedef TIntPoint2<int>      IntPoint2;
-    typedef TIntPoint2<int>      IntPoint2i;
-    typedef TIntPoint2<long>     IntPoint2l;
+	// TIntPoint3
 
-    // TIntPoint3
+	using IntPoint3 = TIntPoint3<int>;
+	using IntPoint3i = TIntPoint3<int>;
+	using IntPoint3l = TIntPoint3<long>;
 
-    typedef TIntPoint3<int>      IntPoint3;
-    typedef TIntPoint3<int>      IntPoint3i;
-    typedef TIntPoint3<long>     IntPoint3l;
+	// TIntPoint4
 
-    // TIntPoint4
+	using IntPoint4 = TIntPoint4<int>;
+	using IntPoint4i = TIntPoint4<int>;
+	using IntPoint4l = TIntPoint4<long>;
 
-    typedef TIntPoint4<int>      IntPoint4;
-    typedef TIntPoint4<int>      IntPoint4i;
-    typedef TIntPoint4<long>     IntPoint4l;
+	// IntVetor2
 
-    // IntVetor2
+	using IntVector2 = TIntVector2<int, false>;
+	using Vector2i = TIntVector2<int, false>;
+	using LongVector2 = TIntVector2<long, false>;
+	using Vector2l = TIntVector2<long, false>;
 
-    typedef TIntVector2<int, false>     IntVector2;
-    typedef TIntVector2<int, false>     Vector2i;
-    typedef TIntVector2<long, false>    LongVector2;
-    typedef TIntVector2<long, false>    Vector2l;
+	// IntVetor3
 
-    // IntVetor3
+	using IntVector3 = TIntVector3<int, false>;
+	using Vector3i = TIntVector3<int, false>;
+	using LongVector3 = TIntVector3<long, false>;
+	using Vector3l = TIntVector3<long, false>;
 
-    typedef TIntVector3<int, false>     IntVector3;
-    typedef TIntVector3<int, false>     Vector3i;
-    typedef TIntVector3<long, false>    LongVector3;
-    typedef TIntVector3<long, false>    Vector3l;
+	// IntVetor4
 
+	using IntVector4 = TIntVector4<int, false>;
+	using Vector4i = TIntVector4<int, false>;
+	using LongVector4 = TIntVector4<long, false>;
+	using Vector4l = TIntVector4<long, false>;
 
-    // IntVetor4
+	// Vector2
 
-    typedef TIntVector4<int, false>     IntVector4;
-    typedef TIntVector4<int, false>     Vector4i;
-    typedef TIntVector4<long, false>    LongVector4;
-    typedef TIntVector4<long, false>    Vector4l;
+	using Vector2 = TVector2<float, false>;
+	using Vector2f = TVector2<float, false>;
+	using Vector2d = TVector2<double, false>;
 
-    // Vector2
+	using Vector2Regf64 = TVector2<double, SIMD::use_simd<double, 2, true>::value>;
+	using Vector2Reg = TVector2<double, SIMD::use_simd<double, 2, true>::value>;
+	using Vector2Regd = TVector2<double, SIMD::use_simd<double, 2, true>::value>;
 
-    typedef TVector2<float, false>      Vector2;
-    typedef TVector2<float, false>      Vector2f;
-    typedef TVector2<double, false>     Vector2d;
+	// Vector3
 
-    typedef TVector2<double, SIMD::use_simd<double, 2, true>::value>        Vector2Regf64;
-    typedef TVector2<double, SIMD::use_simd<double, 2, true>::value>        Vector2Reg;
-    typedef TVector2<double, SIMD::use_simd<double, 2, true>::value>        Vector2Regd;
+	using Vector3 = TVector3<float, false>;
+	using Vector3f = TVector3<float, false>;
+	using Vector3d = TVector3<double, false>;
 
+	using Vector3Reg = TVector3<float, SIMD::use_simd<float, 3, true>::value>;
+	using Vector3Regf32 = TVector3<float, SIMD::use_simd<float, 3, true>::value>;
+	using Vector3Regd = TVector3<double, SIMD::use_simd<double, 3, true>::value>;
+	using Vector3Regf64 = TVector3<double, SIMD::use_simd<double, 3, true>::value>;
 
-    // Vector3
+	// Vector4
 
-    typedef TVector3<float, false>      Vector3;
-    typedef TVector3<float, false>      Vector3f;
-    typedef TVector3<double, false>     Vector3d;
+	using Vector4 = TVector4<float, false>;
+	using Vector4f = TVector4<float, false>;
+	using Vector4d = TVector4<double, false>;
 
-    typedef TVector3<float, SIMD::use_simd<float, 3, true>::value>          Vector3Reg;
-    typedef TVector3<float, SIMD::use_simd<float, 3, true>::value>          Vector3Regf32;
-    typedef TVector3<double, SIMD::use_simd<double, 3, true>::value>        Vector3Regd;
-    typedef TVector3<double, SIMD::use_simd<double, 3, true>::value>        Vector3Regf64;
+	using Vector4Reg = TVector4<float, SIMD::use_simd<float, 4, true>::value>;
+	using Vector4Regf32 = TVector4<float, SIMD::use_simd<float, 4, true>::value>;
+	using Vector4Regd = TVector4<double, SIMD::use_simd<double, 4, true>::value>;
+	using Vector4Regf64 = TVector4<double, SIMD::use_simd<double, 4, true>::value>;
 
+	// Matrix2
 
-    // Vector4
+	using Matrix2 = TMatrix2<float>;
+	using Matrix2f = TMatrix2<float>;
+	using Matrix2d = TMatrix2<double>;
 
-    typedef TVector4<float, false>      Vector4;
-    typedef TVector4<float, false>      Vector4f;
-    typedef TVector4<double, false>     Vector4d;
+	// Matrix3
 
-    typedef TVector4<float, SIMD::use_simd<float, 4, true>::value>          Vector4Reg;
-    typedef TVector4<float, SIMD::use_simd<float, 4, true>::value>          Vector4Regf32;
-    typedef TVector4<double, SIMD::use_simd<double, 4, true>::value>        Vector4Regd;
-    typedef TVector4<double, SIMD::use_simd<double, 4, true>::value>        Vector4Regf64;
-    
-    // Matrix2 
-    
-    typedef TMatrix2<float>             Matrix2;
-    typedef TMatrix2<float>             Matrix2f;
-    typedef TMatrix2<double>            Matrix2d;
+	using Matrix3 = TMatrix3<float, false>;
+	using Matrix3f = TMatrix3<float, false>;
+	using Matrix3d = TMatrix3<double, false>;
 
-    // Matrix3 
+	using Matrix3Reg = TMatrix3<float, SIMD::use_simd<float, 3, true>::value>;
+	using Matrix3Regf = TMatrix3<float, SIMD::use_simd<float, 3, true>::value>;
+	using Matrix3Regd = TMatrix3<double, SIMD::use_simd<double, 3, true>::value>;
+	using Matrix3Regf64 = TMatrix3<double, SIMD::use_simd<double, 3, true>::value>;
 
-    typedef TMatrix3<float, false>      Matrix3;
-    typedef TMatrix3<float, false>      Matrix3f;
-    typedef TMatrix3<double, false>     Matrix3d;
+	// Matrix4
 
-    typedef TMatrix3<float, SIMD::use_simd<float, 3, true>::value>      Matrix3Reg;
-    typedef TMatrix3<float, SIMD::use_simd<float, 3, true>::value>      Matrix3Regf;
-    typedef TMatrix3<double, SIMD::use_simd<double, 3, true>::value>    Matrix3Regd;
-    typedef TMatrix3<double, SIMD::use_simd<double, 3, true>::value>   Matrix3Regf64;
+	using Matrix4 = TMatrix4<float, false>;
+	using Matrix4f = TMatrix4<float, false>;
+	using Matrix4d = TMatrix4<double, false>;
 
-    // Matrix4 
+	using Matrix4Reg = TMatrix3<float, SIMD::use_simd<float, 4, true>::value>;
+	using Matrix4Regf = TMatrix3<float, SIMD::use_simd<float, 4, true>::value>;
+	using Matrix4Regd = TMatrix3<double, SIMD::use_simd<double, 4, true>::value>;
+	using Matrix4Regf64 = TMatrix3<double, SIMD::use_simd<double, 4, true>::value>;
 
-    typedef TMatrix4<float, false>             Matrix4;
-    typedef TMatrix4<float, false>             Matrix4f;
-    typedef TMatrix4<double, false>            Matrix4d;
+	// TPlane
 
-    typedef TMatrix3<float, SIMD::use_simd<float, 4, true>::value>      Matrix4Reg;
-    typedef TMatrix3<float, SIMD::use_simd<float, 4, true>::value>      Matrix4Regf;
-    typedef TMatrix3<double, SIMD::use_simd<double, 4, true>::value>    Matrix4Regd;
-    typedef TMatrix3<double, SIMD::use_simd<double, 4, true>::value>   Matrix4Regf64;
+	using Plane = TPlane<float, false>;
+	using Planef = TPlane<float, false>;
+	using Planed = TPlane<double, false>;
 
+	using PlaneReg = TPlane<float, SIMD::use_simd<float, 4, true>::value>;
+	using PlaneRegd = TPlane<float, SIMD::use_simd<double, 4, true>::value>;
 
-
-    // TPlane 
-
-    typedef TPlane<float, false>        Plane;
-    typedef TPlane<float, false>        Planef;
-    typedef TPlane<double, false>       Planed;
-
-    typedef TPlane<float, SIMD::use_simd<float, 4, true>::value>    PlaneReg;
-    typedef TPlane<float, SIMD::use_simd<double, 4, true>::value>   PlaneRegd;
-
-
-} // Phanes::Core::Math::coretypes
+} // namespace Phanes::Core::Math
 
 namespace Phanes::Core::Math::Internal
 {
 
-    // Internal types
+	// Internal types
 
-    template <typename T, unsigned int D> struct AVector;
+	template <typename T, unsigned int D>
+	struct AVector;
 
-    template <typename T, unsigned int n, unsigned int> struct AMatrix;
-}
-
+	template <typename T, unsigned int n, unsigned int>
+	struct AMatrix;
+} // namespace Phanes::Core::Math::Internal
 
 #endif // !MATH_FWD_H
